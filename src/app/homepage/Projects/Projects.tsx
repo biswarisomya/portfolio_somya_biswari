9f9979e5991dec734def1d90634ee0c6725d6599
Somya Biswari <biswarisomya1@gmail.com>
2023-04-27 17:05:15 -0700

    added weather project details

--- src/app/homepage/Projects/Projects.tsx
+++ src/app/homepage/Projects/Projects.tsx
@@ -8,118 +8,268 @@ import Link from "next/link";
 import { FaGithub } from "react-icons/fa";
 
 const Projects = () => {
-  const [modalIsOpen, setModalIsOpen] = useState(false);
+  const [traveluluModalIsOpen, setTraveluluModalIsOpen] = useState(false);
+  const [weatherModalIsOpen, setWeatherModalIsOpen] = useState(false);
   return (
-    <section id="projects" className=" flex font-sans bg-[#ebeeee]">
-      <div className=" flex flex-col pt-[20px]  pb-[4rem] w-[71%] mx-auto ">
-        <div className=" text-center tracking-wide  desktop:text-[24px] text-[15px] uppercase font-semibold  text-[#95A3A3] mb-[48px]">
+    <section id="projects" className=" flex font-sans bg-[#ebeeee]   ">
+      <div className=" flex flex-col pt-[20px]  pb-[4rem]  w-[71%] mx-auto ">
+        <div className=" w-[100%] text-center tracking-wide  desktop:text-[24px] text-[15px] uppercase font-semibold  text-[#95A3A3] mb-[48px]">
           Projects
         </div>
-        <button onClick={() => setModalIsOpen(true)}>
-          <Image
-            src="/travelulu/desktop-1_50.png"
-            width="300"
-            height="100"
-            alt="Desktop-1"
-          />
-        </button>
-      </div>
-
-      <Modal
-        isOpen={modalIsOpen}
-        onRequestClose={() => setModalIsOpen(false)}
-        contentLabel="Example Modal"
-        className="w-[550px]  mobile:w-[calc(100%_-_40px)] mx-auto mobile:my-[15%] bg-white"
-      >
-        <div className=" text-white bg-black p-[unset] text-white text-center font-bold font-sans px-[10px] py-[10px]">
-          Travelulu
-        </div>
-        <div>
-          <Carousel
-            infiniteLoop
-            interval={1000}
-            autoPlay
-            showIndicators={false}
-            showArrows
-          >
-            <div>
+        <div className="flex  mobile:flex-col   gap-[5rem]">
+          <div className="mobile:flex mobile:justify-center">
+            <button onClick={() => setTraveluluModalIsOpen(true)}>
               <Image
                 src="/travelulu/desktop-1_50.png"
-                width="250"
+                width="300"
                 height="100"
                 alt="Desktop-1"
               />
-              <p className="text-black pt-[10px] pb-[5px]">Desktop-1</p>
-            </div>
-            <div>
-              <Image
-                src="/travelulu/desktop-2_50.png"
-                width="250"
-                height="100"
-                alt="Desktop-2"
-              />
-              <p className="text-black pt-[10px] pb-[5px]">Desktop-2</p>
-            </div>
-            <div>
-              <Image
-                src="/travelulu/mobile-1_50.png"
-                width="250"
-                height="100"
-                alt="Mobile-1"
-              />
-              <p className="text-black pt-[10px] pb-[5px]">Mobile-1</p>
-            </div>
-            <div>
-              <Image
-                src="/travelulu/mobile-2_1_50.png"
-                width="250"
-                height="100"
-                alt="Mobile-2"
-              />
-              <p className="text-black ">Mobile 2</p>
-            </div>
-          </Carousel>
-        </div>
-        <div className="w-[95%] px-[36px] pb-[18px]">
-          <Link
-            className="text-[18px] font-sans font-bold text-[#5F2EEA]"
-            href="https://travelulu.vercel.app/"
-            target="_blank"
-          >
-            Travelulu
-          </Link>
+            </button>
 
-          <div className="text-[#6E7881]">
-            This is a UI template for travel website, using which people can
-            plan their trip ,search hotels and do many more.
+            <Modal
+              isOpen={traveluluModalIsOpen}
+              onRequestClose={() => setTraveluluModalIsOpen(false)}
+              contentLabel="Example Modal"
+              className="w-[550px]  mobile:w-[calc(100%_-_40px)] mx-auto mobile:my-[15%] bg-white"
+            >
+              <div className=" text-white bg-black p-[unset] text-white text-center font-bold font-sans px-[10px] py-[10px]">
+                Travelulu
+              </div>
+              <div>
+                <Carousel
+                  infiniteLoop
+                  interval={1000}
+                  autoPlay
+                  showIndicators={false}
+                  showArrows
+                >
+                  <div>
+                    <Image
+                      src="/travelulu/desktop-1_50.png"
+                      width="250"
+                      height="100"
+                      alt="Desktop-1"
+                    />
+                    <p className="text-black pt-[10px] pb-[5px]">Desktop-1</p>
+                  </div>
+                  <div>
+                    <Image
+                      src="/travelulu/desktop-2_50.png"
+                      width="250"
+                      height="100"
+                      alt="Desktop-2"
+                    />
+                    <p className="text-black pt-[10px] pb-[5px]">Desktop-2</p>
+                  </div>
+                  <div>
+                    <Image
+                      src="/travelulu/mobile-1_50.png"
+                      width="250"
+                      height="100"
+                      alt="Mobile-1"
+                    />
+                    <p className="text-black pt-[10px] pb-[5px]">Mobile-1</p>
+                  </div>
+                  <div>
+                    <Image
+                      src="/travelulu/mobile-2_1_50.png"
+                      width="250"
+                      height="100"
+                      alt="Mobile-2"
+                    />
+                    <p className="text-black ">Mobile-2</p>
+                  </div>
+                </Carousel>
+              </div>
+              <div className="w-[95%] px-[36px] pb-[18px]">
+                <Link
+                  className="text-[18px] font-sans font-bold text-[#5F2EEA]"
+                  href="https://travelulu.vercel.app/"
+                  target="_blank"
+                >
+                  Travelulu
+                </Link>
+
+                <div className="text-[#6E7881]">
+                  This is a UI template for travel website, using which people
+                  can plan their trip ,search hotels and do many more.
+                </div>
+                <div className="font-bold mt-[10px]">Tech Stack:</div>
+                <div className=" text-[#6E7881] font-bold font-sans">
+                  Next JS, Tailwind, React,TypeScript, HTML 5,CSS 3 ,Vercel
+                </div>
+              </div>
+              <div className=" items-center bg-black p-[unset] gap-[1rem] flex text-white font-bold font-sans px-[2rem] py-[1rem] ">
+                <Link
+                  className=""
+                  href="https://github.com/biswarisomya/travelulu"
+                  target="_blank"
+                >
+                  <FaGithub />
+                </Link>
+                <Link href="https://travelulu.vercel.app/" target="_blank">
+                  Demo
+                </Link>
+                <Link
+                  href="https://www.figma.com/file/yEx8JfvrJv73ozazLSiowf/Travelulu---Travel-Planning-UI-Kit?node-id=99-269&t=16bnngB94mO2WcKQ-0"
+                  target="_blank"
+                >
+                  Figma
+                </Link>
+                <button
+                  className="uppercase"
+                  onClick={() => setTraveluluModalIsOpen(false)}
+                >
+                  close
+                </button>
+              </div>
+            </Modal>
           </div>
-          <div className="font-bold mt-[10px]">Tech Stack:</div>
-          <div className=" text-[#6E7881] font-bold font-sans">
-            Next JS, Tailwind, React,TypeScript, HTML,CSS 3 ,Vercel
+          <div className="mobile:flex mobile:justify-center">
+            <button onClick={() => setWeatherModalIsOpen(true)}>
+              <Image
+                className="h-[unset] h-[10rem] "
+                src="/weather/desktop-2.png"
+                width="300"
+                height="100"
+                alt="Desktop-1"
+              />
+            </button>
+
+            <Modal
+              isOpen={weatherModalIsOpen}
+              onRequestClose={() => setWeatherModalIsOpen(false)}
+              contentLabel="Example Modal"
+              className="w-[550px]  mobile:w-[calc(100%_-_40px)] mx-auto mobile:my-[15%] bg-white"
+            >
+              <div className=" text-white bg-black p-[unset] text-white text-center font-bold font-sans px-[10px] py-[10px]">
+                Weather
+              </div>
+              <div>
+                <Carousel
+                  infiniteLoop
+                  interval={1000}
+                  autoPlay
+                  showIndicators={false}
+                  showArrows
+                >
+                  {/*<div>*/}
+                  {/*  <Image*/}
+                  {/*    src="/weather/desktop-1.png"*/}
+                  {/*    width="250"*/}
+                  {/*    height="50"*/}
+                  {/*    alt="Desktop-1"*/}
+                  {/*  />*/}
+                  {/*  <p className="text-black pt-[10px] pb-[5px]">Desktop-1</p>*/}
+                  {/*</div>*/}
+                  <div>
+                    <Image
+                      src="/weather/desktop-2.png"
+                      width="250"
+                      height="100"
+                      alt="Desktop-1"
+                    />
+                    <p className="text-black pt-[10px] pb-[5px]">Desktop-1</p>
+                  </div>
+                  <div>
+                    <Image
+                      src="/weather/desktop-3.png"
+                      width="250"
+                      height="100"
+                      alt="Desktop-2"
+                    />
+                    <p className="text-black pt-[10px] pb-[5px]">Desktop-2</p>
+                  </div>
+                  <div>
+                    <Image
+                      src="/weather/desktop-4.png"
+                      width="250"
+                      height="100"
+                      alt="Desktop-3"
+                    />
+                    <p className="text-black ">Desktop-3</p>
+                  </div>
+                  <div>
+                    <Image
+                      src="/weather/desktop-5.png"
+                      width="250"
+                      height="100"
+                      alt="Desktop-4"
+                    />
+                    <p className="text-black ">Desktop-4</p>
+                  </div>
+                  <div>
+                    <Image
+                      src="/weather/mobile-3.png"
+                      width="250"
+                      height="100"
+                      alt="Mobile-1"
+                    />
+                    <p className="text-black ">Mobile-1</p>
+                  </div>
+                  <div>
+                    <Image
+                      src="/weather/mobile-4.png"
+                      width="250"
+                      height="100"
+                      alt="Mobile-2"
+                    />
+                    <p className="text-black ">Mobile-2</p>
+                  </div>
+                </Carousel>
+              </div>
+              <div className="w-[95%] px-[36px] pb-[18px] mobile:px-[2px] mobile:pb-[2px]">
+                <Link
+                  className="text-[18px] font-sans font-bold text-[#5F2EEA]"
+                  href="https://weather-app-biswarisomya.vercel.app/"
+                  target="_blank"
+                >
+                  Weather
+                </Link>
+
+                <div className="text-[#6E7881]">
+                  This is a UI template for Weather website, using which people
+                  can get the Real time weather, Timezone, Astronomy and Sports
+                  for a given city anywhere across the globe!
+                </div>
+                <div className="font-bold mt-[10px]">Tech Stack:</div>
+                <div className=" text-[#6E7881] font-bold font-sans">
+                  React JS, TypeScript, HTML 5, CSS 3, SCSS, Axios,
+                  React-Router,Vercel
+                </div>
+              </div>
+              <div className=" items-center bg-black p-[unset] gap-[1rem] flex text-white font-bold font-sans px-[2rem] py-[1rem] ">
+                <Link
+                  className=""
+                  href="https://github.com/biswarisomya/weather-app"
+                  target="_blank"
+                >
+                  <FaGithub />
+                </Link>
+                <Link
+                  href="https://weather-app-biswarisomya.vercel.app/"
+                  target="_blank"
+                >
+                  Demo
+                </Link>
+                {/*<Link*/}
+                {/*  href="https://www.figma.com/file/yEx8JfvrJv73ozazLSiowf/Travelulu---Travel-Planning-UI-Kit?node-id=99-269&t=16bnngB94mO2WcKQ-0"*/}
+                {/*  target="_blank"*/}
+                {/*>*/}
+                {/*  Figma*/}
+                {/*</Link>*/}
+                <button
+                  className="uppercase"
+                  onClick={() => setWeatherModalIsOpen(false)}
+                >
+                  close
+                </button>
+              </div>
+            </Modal>
           </div>
         </div>
-        <div className=" items-center bg-black p-[unset] gap-[1rem] flex text-white font-bold font-sans px-[2rem] py-[1rem] ">
-          <Link
-            className=""
-            href="https://github.com/biswarisomya/travelulu"
-            target="_blank"
-          >
-            <FaGithub />
-          </Link>
-          <Link href="https://travelulu.vercel.app/" target="_blank">
-            Demo
-          </Link>
-          <Link
-            href="https://www.figma.com/file/yEx8JfvrJv73ozazLSiowf/Travelulu---Travel-Planning-UI-Kit?node-id=99-269&t=16bnngB94mO2WcKQ-0"
-            target="_blank"
-          >
-            Figma
-          </Link>
-          <button className="uppercase" onClick={() => setModalIsOpen(false)}>
-            close
-          </button>
-        </div>
-      </Modal>
+      </div>
     </section>
   );
 };
--- src/app/homepage/Resume/Skills/Skills.tsx
+++ src/app/homepage/Resume/Skills/Skills.tsx
@@ -4,6 +4,92 @@ import SectionLayout, {
 } from "@/app/components/SectionLayout/SectionLayout";
 
 const Skills = () => {
+  const data = [
+    {
+      technology: "HTML 5",
+      percentage: "90%",
+    },
+    {
+      technology: "CSS 3",
+      percentage: "90",
+    },
+    {
+      technology: "TYPESCRIPT",
+      percentage: "90",
+    },
+    {
+      technology: "JAVASCRIPT",
+      percentage: "90",
+    },
+    {
+      technology: "REACT JS",
+      percentage: "90",
+    },
+    {
+      technology: "REDUX",
+      percentage: "90",
+    },
+    {
+      technology: "BOOTSTRAP",
+      percentage: "90",
+    },
+    {
+      technology: "TAILWIND",
+      percentage: "90",
+    },
+    {
+      technology: "SCSS",
+      percentage: "90",
+    },
+    {
+      technology: "REACT TESTING LIBRARY",
+      percentage: "90",
+    },
+    {
+      technology: "JEST",
+      percentage: "90",
+    },
+    {
+      technology: "GIT",
+      percentage: "90",
+    },
+    {
+      technology: "NODE JS",
+      percentage: "90",
+    },
+    {
+      technology: "NEXT JS",
+      percentage: "80%",
+    },
+    {
+      technology: "EXPRESS JS",
+      percentage: "80%",
+    },
+    {
+      technology: "MONGO DB",
+      percentage: "80%",
+    },
+    {
+      technology: "SQL",
+      percentage: "80%",
+    },
+    {
+      technology: "AJAX",
+      percentage: "80%",
+    },
+    {
+      technology: "REST API",
+      percentage: "80%",
+    },
+    {
+      technology: "NEXT JS",
+      percentage: "80%",
+    },
+    {
+      technology: "J-QUERY",
+      percentage: "70%",
+    },
+  ];
   return (
     <SectionLayout className=" mobile:mt-[3rem] ">
       <Heading>skills</Heading>
@@ -39,106 +125,20 @@ const Skills = () => {
           </div>
         </div>
         <div className="uppercase tracking-widest font-sans font-bold">
-          <div>
-            <p className="mobile:text-left mobile:tracking-widest mb-[15px] ">
-              Javascript
-            </p>
-            <div className="w-[100%] bg-[#ccc] mb-[15px] ">
-              <div className=" h-[42px] w-[90%] bg-[#313131] text-white text-right">
-                90%
+          {data.map((d) => {
+            return (
+              <div>
+                <p className="mobile:text-left mobile:tracking-widest mb-[15px] ">
+                  {d.technology}
+                </p>
+                <div className="w-[100%] bg-[#ccc] mb-[15px] ">
+                  <div className=" h-[42px] w-[90%] bg-[#313131] text-white text-right">
+                    {d.percentage}
+                  </div>
+                </div>
               </div>
-            </div>
-          </div>
-          <div>
-            <p className="mobile:text-left mobile:tracking-widest mb-[15px]">
-              Node js
-            </p>
-            <div className="w-[100%] bg-[#ccc] mb-[15px] ">
-              <div className=" h-[42px] w-[80%] bg-[#313131] text-white text-right">
-                80%
-              </div>
-            </div>
-          </div>
-          <div>
-            <p className="mobile:text-left mobile:tracking-widest mb-[15px]">
-              React + Redux + Mobx
-            </p>
-            <div className="w-[100%] bg-[#ccc] mb-[15px]">
-              <div className=" h-[42px] w-[90%] bg-[#313131] text-white text-right">
-                90%
-              </div>
-            </div>
-          </div>
-          <div>
-            <p className="mobile:text-left mobile:tracking-widest mb-[15px]">
-              Angular JS
-            </p>
-            <div className="w-[100%] bg-[#ccc] mb-[15px] ">
-              <div className=" h-[42px] w-[30%] bg-[#313131] text-white text-right">
-                30%
-              </div>
-            </div>
-          </div>
-          <div>
-            <p className="mobile:text-left mobile:tracking-widest mb-[15px]">
-              Java
-            </p>
-            <div className="w-[100%] bg-[#ccc] mb-[15px] ">
-              <div className=" h-[42px] w-[60%] bg-[#313131] text-white text-right">
-                60%
-              </div>
-            </div>
-          </div>
-          <div>
-            <p className="mobile:text-left mobile:tracking-widest mb-[15px]">
-              Oracle
-            </p>
-            <div className="w-[100%] bg-[#ccc] mb-[15px] ">
-              <div className=" h-[42px] w-[70%] bg-[#313131] text-white text-right">
-                70%
-              </div>
-            </div>
-          </div>
-          <div>
-            <p className="mobile:text-left mobile:tracking-widest mb-[15px]">
-              CSS
-            </p>
-            <div className="w-[100%] bg-[#ccc]  mb-[15px]">
-              <div className=" h-[42px] w-[90%] bg-[#313131] text-white text-right">
-                90%
-              </div>
-            </div>
-          </div>
-          <div>
-            <p className="mobile:text-left mobile:tracking-widest mb-[15px]">
-              HTML 5
-            </p>
-            <div className="w-[100%] bg-[#ccc] mobile:mb-[15px] ">
-              <div className=" h-[42px] w-[90%] bg-[#313131] text-white text-right">
-                90%
-              </div>
-            </div>
-          </div>
-          <div>
-            <p className="mobile:text-left mobile:tracking-widest mb-[15px]">
-              Bootstrap
-            </p>
-            <div className="w-[100%] bg-[#ccc] mb-[15px]">
-              <div className=" h-[42px] w-[90%] bg-[#313131] text-white text-right">
-                90%
-              </div>
-            </div>
-          </div>
-          <div>
-            <p className="mobile:text-left mobile:tracking-widest mb-[15px]">
-              MongoDB
-            </p>
-            <div className="w-[100%] bg-[#ccc] mb-[15px]">
-              <div className=" h-[42px] w-[80%] bg-[#313131] text-white text-right">
-                80%
-              </div>
-            </div>
-          </div>
+            );
+          })}
         </div>
       </Content>
     </SectionLayout>
